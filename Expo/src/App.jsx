26a8efc499c6c473fd92b0eb1e72f5e40ba75512
Samuel Narvaja Torres <samuel.narvaja.torres@gmail.com>
2024-09-15 23:09:12 -0300

    1.0.4

--- Expo/src/App.jsx
+++ Expo/src/App.jsx
@@ -1,25 +1,81 @@
-import { useState, useEffect, useRef } from 'react';
-import Draggable from 'react-draggable';
-import './App.css';
+import { useState, useEffect, useRef } from "react";
+import Draggable from "react-draggable";
+import "animate.css";
+import "./App.css";
 
 const imageData = [
-  { id: 'img1', src: '/ExpoLidercom/assets/Imagen/Intersec.jpeg', info: 'Información sobre imagen 1', category: 'Imagen' },
-  { id: 'img2', src: '/ExpoLidercom/assets/Imagen/Programador.jpg', info: 'Información sobre imagen 2', category: 'Imagen' },
-  { id: 'img3', src: '/ExpoLidercom/assets/Seguridad/Alarma.jpg', info: 'Información sobre imagen 3', category: 'Seguridad' },
-  { id: 'img4', src: '/ExpoLidercom/assets/Seguridad/Unicon.jpg', info: 'Información sobre imagen 4', category: 'Seguridad' },
-  { id: 'img5', src: '/ExpoLidercom/assets/Seguridad/X28.jpg', info: 'Información sobre imagen 5', category: 'Seguridad' },
-  { id: 'img6', src: '/ExpoLidercom/assets/Telecomunicaciones/Camara.jpg', info: 'Información sobre imagen 6', category: 'Telecomunicaciones' },
-  { id: 'img7', src: '/ExpoLidercom/assets/Telecomunicaciones/Camara2.jpg', info: 'Información sobre imagen 7', category: 'Telecomunicaciones' },
-  { id: 'img8', src: '/ExpoLidercom/assets/Telecomunicaciones/Compu.jpg', info: 'Información sobre imagen 8', category: 'Telecomunicaciones' },
-  { id: 'img9', src: '/ExpoLidercom/assets/Telecomunicaciones/Startlink.jpg', info: 'Información sobre imagen 9', category: 'Telecomunicaciones' },
-  { id: 'img10', src: '/ExpoLidercom/assets/Telecomunicaciones/Trabajo.jpg', info: 'Información sobre imagen 10', category: 'Telecomunicaciones' },
-  {id: 'img11', src: '/ExpoLidercom/assets/Imagen/Cat.gif', info: 'Información sobre imagen 11', category: 'Imagen' },
+  {
+    id: "img1",
+    src: "/ExpoLidercom/assets/Imagen/Intersec.jpeg",
+    info: "Información sobre imagen 1",
+    category: "Imagen",
+  },
+  {
+    id: "img2",
+    src: "/ExpoLidercom/assets/Imagen/Programador.jpg",
+    info: "Información sobre imagen 2",
+    category: "Imagen",
+  },
+  {
+    id: "img3",
+    src: "/ExpoLidercom/assets/Seguridad/Alarma.jpg",
+    info: "Información sobre imagen 3",
+    category: "Seguridad",
+  },
+  {
+    id: "img4",
+    src: "/ExpoLidercom/assets/Seguridad/Unicon.jpg",
+    info: "Información sobre imagen 4",
+    category: "Seguridad",
+  },
+  {
+    id: "img5",
+    src: "/ExpoLidercom/assets/Seguridad/X28.jpg",
+    info: "Información sobre imagen 5",
+    category: "Seguridad",
+  },
+  {
+    id: "img6",
+    src: "/ExpoLidercom/assets/Telecomunicaciones/Camara.jpg",
+    info: "Información sobre imagen 6",
+    category: "Telecomunicaciones",
+  },
+  {
+    id: "img7",
+    src: "/ExpoLidercom/assets/Telecomunicaciones/Camara2.jpg",
+    info: "Información sobre imagen 7",
+    category: "Telecomunicaciones",
+  },
+  {
+    id: "img8",
+    src: "/ExpoLidercom/assets/Telecomunicaciones/Compu.jpg",
+    info: "Información sobre imagen 8",
+    category: "Telecomunicaciones",
+  },
+  {
+    id: "img9",
+    src: "/ExpoLidercom/assets/Telecomunicaciones/Startlink.jpg",
+    info: "Información sobre imagen 9",
+    category: "Telecomunicaciones",
+  },
+  {
+    id: "img10",
+    src: "/ExpoLidercom/assets/Telecomunicaciones/Trabajo.jpg",
+    info: "Información sobre imagen 10",
+    category: "Telecomunicaciones",
+  },
+  {
+    id: "img11",
+    src: "/ExpoLidercom/assets/Imagen/Cat.gif",
+    info: "Información sobre imagen 11",
+    category: "Imagen",
+  },
 ];
 
 const App = () => {
   const [selectedOption, setSelectedOption] = useState(null);
   const [images, setImages] = useState([]);
-  const [visibleInfo, setVisibleInfo] = useState(null);
+  const [flippedCards, setFlippedCards] = useState({});
   const touchStartTime = useRef(0);
   const touchMoved = useRef(false);
 
@@ -27,23 +83,30 @@ const App = () => {
     if (selectedOption) {
       const filteredImages = imageData
         .filter((image) => image.category === selectedOption)
-        .map((image, index) => ({
+        .map((image) => ({
           ...image,
-          position:getRandomPosition(index),
+          position: getRandomPosition(),
         }));
       setImages(filteredImages);
     }
-  }, [selectedOption]);
+    console.log("Estado flippedCards actualizado:", flippedCards);
+  }, [selectedOption, flippedCards]);
 
-  const getRandomPosition = (index) => {
-    // Ajusta estos valores para controlar la dispersión
-    const maxX = window.innerWidth - 250; // Ancho de la ventana menos el ancho de la imagen
-    const maxY = window.innerHeight - 250; // Altura de la ventana menos algo de espacio
-  
-    const randomX = Math.floor(Math.random() * maxX); // Posición aleatoria en X
-    const randomY = Math.floor(Math.random() * maxY); // Posición aleatoria en Y
-  
-    return { x: randomX, y: randomY }; // Devuelve la posición dispersa
+  const getRandomPosition = () => {
+    const maxX = window.innerWidth - 250;
+    const maxY = window.innerHeight - 250;
+
+    const randomX = Math.floor(Math.random() * maxX);
+    const randomY = Math.floor(Math.random() * maxY);
+
+    return { x: randomX, y: randomY };
+  };
+
+  const handleFlip = (id) => {
+    setFlippedCards((prevState) => ({
+      ...prevState,
+      [id]: !prevState[id], // Alterna el estado de la imagen actual
+    }));
   };
 
   const handleOptionClick = (option) => {
@@ -51,7 +114,10 @@ const App = () => {
   };
 
   const handleDoubleClick = (id) => {
-    setVisibleInfo(visibleInfo === id ? null : id);
+    setFlippedCards((prevState) => ({
+      ...prevState,
+      [id]: !prevState[id], // Alterna el estado de la imagen actual
+    }));
   };
 
   const handleTouchStart = () => {
@@ -66,56 +132,78 @@ const App = () => {
   const handleTouchEnd = (id) => {
     const touchDuration = Date.now() - touchStartTime.current;
     if (!touchMoved.current && touchDuration < 500) {
-      // Consideramos esto como un toque simple
-      setVisibleInfo((prevVisibleInfo) =>
-        prevVisibleInfo === id ? null : id
-      );
+      console.log(`Touched image with id: ${id}`);
+      handleFlip(id);
     }
   };
 
   const handleBackClick = () => {
     setSelectedOption(null);
-    setVisibleInfo(null);
+    setFlippedCards({});
   };
 
   return (
     <div className="app">
       <div className="logo-container">
-        <img src="/ExpoLidercom/assets/LogoTextoAzul.png" alt="Logo" className="logo" />
+        <img
+          src="/ExpoLidercom/assets/LogoTextoAzul.png"
+          alt="Logo"
+          className="logo"
+        />
       </div>
       {!selectedOption ? (
         <div className="start-screen">
           <h1 className="start-screen-title">Pantalla de Inicio</h1>
           <div className="buttons-container">
-            <button onClick={() => handleOptionClick('Telecomunicaciones')} className="option-button">Telecomunicaciones</button>
-            <button onClick={() => handleOptionClick('Imagen')} className="option-button">Imagen</button>
-            <button onClick={() => handleOptionClick('Seguridad')} className="option-button">Seguridad</button>
+            <button
+              onClick={() => handleOptionClick("Telecomunicaciones")}
+              className="option-button"
+            >
+              Telecomunicaciones
+            </button>
+            <button
+              onClick={() => handleOptionClick("Imagen")}
+              className="option-button"
+            >
+              Imagen
+            </button>
+            <button
+              onClick={() => handleOptionClick("Seguridad")}
+              className="option-button"
+            >
+              Seguridad
+            </button>
           </div>
         </div>
       ) : (
         <div className="images-container">
-          <button onClick={handleBackClick} className="back-button">Volver</button>
+          <button onClick={handleBackClick} className="back-button">
+            Volver
+          </button>
           {images.map((image, index) => (
             <Draggable
               key={image.id}
               defaultPosition={image.position}
               bounds="parent"
             >
-              <div className="image-wrapper">
-                <div 
-                  className={`image-box floating floating-${index % 3}`}
+              <div className={`image-wrapper`}>
+                <div
+                  className={`image-box ${
+                    flippedCards[image.id] ? "flipped" : ""
+                  }`}
+                  style={{ animationDelay: `${index * 0.4}s` }}
                   onDoubleClick={() => handleDoubleClick(image.id)}
                   onTouchStart={handleTouchStart}
                   onTouchMove={handleTouchMove}
                   onTouchEnd={() => handleTouchEnd(image.id)}
                 >
-                  <img src={image.src} alt={image.info} className="image" />
-                </div>
-                {visibleInfo === image.id && (
-                  <div className="info-box">
-                    <p>{image.info}</p>
+                  <div className="image-content">
+                    <img src={image.src} alt={image.info} className="image" />
+                    <div className="info-box">
+                      <p>{image.info}</p>
+                    </div>
                   </div>
-                )}
+                </div>
               </div>
             </Draggable>
           ))}
